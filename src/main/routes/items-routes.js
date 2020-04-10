aa6026ed482f40a51c9a051a495cf06a064d853f
Filipe Macedo <devmacedo@gmail.com>
2020-04-10 09:34:51 -0300

    feat: call method of items controller

--- src/main/routes/items-routes.js
+++ src/main/routes/items-routes.js
@@ -1,5 +1,12 @@
+const ItemsController = require('../../presentation/controllers/items');
+const MercadoLibreService = require('../../infra/services/mercado-libre');
+
+const itemController = new ItemsController(new MercadoLibreService());
+
 module.exports = (router) => {
-	router.get('/items', (req, res) => {
-		res.json({ message: 'ok ' });
+	router.get('/items', async (req, res) => {
+		const response = await itemController.get();
+
+		return res.status(response.statusCode).json(response);
 	});
 };
